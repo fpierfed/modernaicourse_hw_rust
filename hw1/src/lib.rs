03088a458c51631eb63f2d6f5d80ada2d4846e4c
Francesco Pierfederici <fpierfed@gmail.com>
2026-05-15 13:26:28 +0200

    implemented matmul_3

--- hw1/src/lib.rs
+++ hw1/src/lib.rs
@@ -343,8 +343,27 @@ where
 ///
 /// Output:
 ///     2D array (m x p) - matrix AB
-pub fn matmul_3(a: &Array2<f32>, b: &Array2<f32>) -> Array2<f32> {
-    todo!()
+pub fn matmul_3<S1, S2>(a: &ArrayBase<S1, Ix2>, b: &ArrayBase<S2, Ix2>) -> Array2<f32>
+where
+    S1: Data<Elem = f32>,
+    S2: Data<Elem = f32>,
+{
+    assert_eq!(
+        a.shape()[1],
+        b.shape()[0],
+        "Expecting compatible dimenstions!"
+    );
+
+    let m = a.shape()[0];
+    let p = b.shape()[1];
+    let mut result = Array2::zeros((m, p));
+
+    for (i, mut res_row) in result.rows_mut().into_iter().enumerate() {
+        let row = vector_matrix_product_2(&a.row(i), b);
+        res_row.assign(&row);
+    }
+
+    result
 }
 
 /*
