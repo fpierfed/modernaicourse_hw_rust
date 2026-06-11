3b6847eb36eacff495acd94416f1773fa963cc76
Francesco Pierfederici <fpierfed@gmail.com>
2026-06-11 11:25:40 +0200

    initial

---
+++ hw5/src/bin/train_small.rs
@@ -0,0 +1,25 @@
+use candle_core::{Device, Result, Tensor};
+use hw5::*;
+use std::path::Path;
+
+fn main() -> Result<()> {
+    let device: Device = default_device()?;
+    let token_path = Path::new("TinyStoriesV2-GPT4-train.small.bin");
+
+    pretokenize_tinystories(token_path)?;
+
+    let loader = DataLoader::new(token_path, 512, 8, &device)?;
+
+    // GPT-2's vocab size is 50257, so:
+    // - 50257 // 256 = 196
+    // - (196 + 1) * 256 = 50432
+    let mut model = LLM::new(50432, 256, 8, 512, 512, 4, &device)?;
+
+    let mut opt = Adam::new(model.parameters(), 1.0e-3, (0.9, 0.95), 1.0e-8)?;
+    {
+        // Set the KV Cache to false during training / to true during inference.
+        let mut step = |x: &Tensor| model.forward(x, 0, false);
+        train_llm(&mut step, loader, &mut opt)?;
+    }
+    Ok(())
+}
